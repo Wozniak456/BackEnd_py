c0e3d9095c460e454cdc7eca4a12e9d10d7f7cab
Sofiia <voznyakson@gmail.com>
2022-10-18 17:09:04 +0300

    New views added: users and records

--- backEnd_1/__init__.py
+++ backEnd_1/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 from backEnd_1 import views
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
--- backEnd_1/views.py
+++ backEnd_1/views.py
@@ -1,6 +1,6 @@
 from flask import jsonify, request
 from backEnd_1 import app
-
+import datetime
 CATEGORIES = [
     {
         "id": 1,
@@ -8,8 +8,25 @@ CATEGORIES = [
     },
 ]
 
-# GET /categories
-# POST /category
+USERS = [
+    {
+        "id": 1,
+        "name": "Sofiia Vozniak",
+    },
+]
+
+RECORDS = [
+    {
+        "id": 1,
+        "userID": 1,
+        "categoryID": 1,
+        "date": datetime.datetime.now(),
+        "cost": 100,
+    },
+]
+
+# GET /categories, /users, /records
+# POST /category, /user, /record
 
 @app.route("/categories")
 def get_categories():
@@ -19,4 +36,24 @@ def get_categories():
 def create_category():
     request_data = request.get_json()
     CATEGORIES.append(request_data)
-    return jsonify({"status": "ok"})
+    return jsonify({request_data})
+
+@app.route("/users")
+def get_users():
+    return jsonify({"users": USERS})
+
+@app.route("/user", methods=['POST'])
+def create_user():
+    request_data = request.get_json()
+    USERS.append(request_data)
+    return jsonify({request_data})
+
+@app.route("/records")
+def get_records():
+    return jsonify({"records": RECORDS})
+
+@app.route("/record", methods=['POST'])
+def create_record():
+    request_data = request.get_json()
+    RECORDS.append(request_data)
+    return jsonify({request_data})
